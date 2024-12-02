1a9e2bbbbb8cd8f398f6028acc5e4aa4ab13ad27
Gabriel Speer <gspeer2@ut.utm.edu>
2024-12-02 21:15:12 +0000

    tokenizing stage

---
+++ tokenize_corpus.py
@@ -0,0 +1,18 @@
+import tiktoken
+import numpy as np
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+with open("corpus.txt", "r") as file:
+    text = file.read()
+
+tokens = tokenizer.encode(text)
+
+np.save("tokens.npy", tokens)
+
+# Split into training and validation sets (90/10)
+train_tokens = tokens[:int(len(tokens) * 0.9)]
+val_tokens = tokens[int(len(tokens) * 0.9):]
+
+np.save("train_tokens.npy", train_tokens)
+np.save("val_tokens.npy", val_tokens)
